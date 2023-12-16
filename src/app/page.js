9931ec59822b01c4f523b754f484c8ea0836aed7
M3BIONIX <you@example.com>
2023-12-16 16:47:16 +0530

    vercel changes

--- src/app/page.js
+++ src/app/page.js
@@ -18,7 +18,7 @@ export default function Home() {
     
     <div className="flex-1 bg-custom-black w-screen h-screen">
     <div className="flex justify-between px-4 xs:ml-[30px]">
-      <h4 className="pt-16 font-product-sans text-custom-red tracking-wide text-3xl xs:text-xl md:text-4xl  xs:justify-start">M3BIONIX</h4>
+      <h4 className="md:pt-16 xs:pt-10 font-product-sans text-custom-red tracking-wide text-3xl xs:text-xl md:text-4xl  xs:justify-start">M3BIONIX</h4>
       <div className="flex pt-16 text-right">
         <div className="group transition-all duration-300 ease-in-out px-5 md:flex hidden">
           <button className="bg-left-bottom font-product-sans-m py-3 bg-gradient-to-r from-custom-red to-custom-red bg-[length:0%_2px] bg-no-repeat group-hover:bg-[length:100%_2px] transition-all duration-500 ease-out">
@@ -39,7 +39,7 @@ export default function Home() {
         </div>
       </div>  
 
-      <div className="flex pt-16 md:hidden mr-5">
+      <div className="flex md:pt-16 xs:pt-10 md:hidden mr-5">
         <button className="jmr-3" onClick={() => setIsOpen(!isOpen)}>
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="h-6 w-6">
               <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h16M4 18h16" />
@@ -76,7 +76,7 @@ export default function Home() {
     <div className="md:text-center xs:right-10 flex xs:items-center justify-center md:h-auto ">
       <p className="font-product-sans text-custom-red xs:text-5xl md:text-7xl xs:mt-[20px] md:mt-10 transition-colors duration-300 hover:text-white">Hey, there</p>        
     </div>
-      <div className="w-full mt-8 hidden md:flex mx-auto sm:w-1/3 md:w-1/2 md:mr-[10rem]">
+      <div className="w-full mt-8 hidden md:flex mx-auto sm:w-1/3 md:w-1/2 md:mr-[180px]">
           <p className="font-product-sans-m md:leading-8 text-left xs:text-sm xs:text-justify md:text-base ">
           I'm Sanjay, fluent in the languages of movies, anime, and questionable code.
           I dabble in philosophy, mostly to fuel my existential dread during ramen nights.
@@ -84,17 +84,19 @@ export default function Home() {
           Hire me before I code my own AI girlfriend (no judgment on my waifu preferences).
           </p>
       </div>
-      <div className="w-full mt-8 md:hidden px-4">
-        <p className="font-product-sans-m xs:text-sm xs:text-center xs:justify-around xs:w-auto md:text-base ">
-          {joke}
-        </p>
-      </div>
-      <button type="button" className="hire hidden md:flex items-center">
-        Hire Me
-        <svg className="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
-          <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
-        </svg>
-      </button>
+      <div className="md:flex md:justify-between">
+  <div className="w-full mt-8 md:hidden px-4">
+    <p className="font-product-sans-m xs:text-sm xs:text-center xs:justify-around xs:w-auto md:text-base ">
+      {joke}
+    </p>
+  </div>
+  <button type="button" className="hire flex items-center xs:w-auto xs:px-4 xs:py-2 md:py-4 md:px-5 xs:mx-auto xs:text-center md:mr-[15rem]" >
+  <span className="xs:text-sm sm:text-base">Hire Me</span>
+  <svg className="rtl:rotate-180 xs:w-2.5 sm:w-3.5 xs:h-2.5 sm:h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+    <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
+  </svg>
+</button>
+</div>
       
       <div className="gooey absolute md:left-10 md:bottom-10 md:w-[350px] md:h-[400px] xs:flex xs:justify-center xs:left-[7rem] xs:bottom-0 xs:w-[150px] xs:h-[200px]" />
       <div className="absolute md:left-0 md:bottom-0 md:ml-10 z-2 filter grayscale hover:grayscale-0 transition-all duration-500 xs:flex xs:justify-center xs:left-0 xs:bottom-0 ">
