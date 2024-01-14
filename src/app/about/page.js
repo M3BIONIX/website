7001dfd6d7944555f99cdc94a84bea7a8be8baa6
M3BIONIX <you@example.com>
2024-01-15 03:39:31 +0530

    about page done

---
+++ src/app/about/page.js
@@ -0,0 +1,123 @@
+export default function About() {
+   return(
+      <div className="bg-custom-black min-h-screen flex flex-col items-center">
+         <p className="font-product-sans text-custom-red transition-colors duration-300 hover:text-white xs:self-center xs:text-4xl xs:mt-[50px] md:self-start md:ml-[50px] md:mt-[50px] md:text-7xl ">About Me</p>
+         <div className="w-full flex justify-center">
+            <img src="/images/image.svg" className="md:h-[400px] md:mt-[20px] xs:h-[300px]" />
+         </div>
+         <div className="font-product-sans-m md:leading-8 justify-center md:text-base md:w-1/2 xs:w-3/4 text-center">
+               You've clicked the "About Me" button? Bold move, bro. 
+               <p className="hidden md:block"> Like, did you accidentally bump into it with your elbow while frantically refreshing your Netflix queue, or are you actually 
+               curious about the magnificent beast lurking behind this digital curtain? Anyways
+               </p>
+         </div>
+
+         <div className="flex flex-col md:items-start xs:items-center">
+         <p className="font-product-sans pulse text-green-600 transition-colors duration-300 hover:text-white xs:self-center xs:text-4xl xs:mt-[50px] md:self-start md:mt-[50px] md:text-3xl ">Vibing to</p>
+         <img src="https://spotify-black-five.vercel.app/api/spotify" alt="Spotify Playing" width="350" className="mt-[20px] xs:w-[200px] md:w-[350px]"/>
+         </div>
+
+         <div className="md:grid md:grid-cols-2 md:gap-4 w-screen justify-center md:mt-[60px] ">
+            <div className="flex flex-col md:items-start xs:items-center md:ml-[13rem]  xs:space-y-5">
+               <p className="font-product-sans text-custom-red transition-colors duration-300 hover:text-white xs:self-center xs:text-4xl xs:mt-[50px] md:self-start md:ml-[80px] md:mt-[50px] md:text-3xl ">Education</p>
+               
+               <div className="flex flex-row items-center xs:space-x-5">
+                  <img src="/images/college.svg" className="md:h-[40px] md:mt-[40px] md:ml-[80px] border-[2px] rounded-full p-[3px] xs:h-[40px]" />
+                  <div className="flex flex-col md:ml-[20px] md:mt-[40px]">
+                     <p className="font-product-sans-m ">College of Engineering Chengannur</p>
+                     <p className="font-product-sans-m text-[11px]">2020 - 2024</p>
+                  </div>
+               </div>
+
+               <div className="flex flex-row items-center xs:space-x-5 xs:ml-[15px] md:ml-[0px]">
+                  <img src="/images/school.svg" className="md:h-[40px] md:mt-[40px] md:ml-[80px] border-[2px] rounded-full p-[6px] xs:h-[40px]" />
+                  <div className="flex flex-col md:ml-[20px] md:mt-[40px]">
+                     <p className="font-product-sans-m ">Saraswathi Vidyaniketan Public School</p>
+                     <p className="font-product-sans-m text-[11px]">2018 - 2020</p>
+                  </div>
+               </div>
+
+               <div className="flex flex-row items-center xs:space-x-5 xs:mr-[45px]">
+               <img src="/images/hs.svg" className="md:h-[40px] md:mt-[40px] md:ml-[80px] border-[2px] rounded-full p-[6px] xs:h-[40px]" />
+                 <div className="flex flex-col md:ml-[20px] md:mt-[40px]">
+                     <p className="font-product-sans-m ">Viswajyothi CMI Public School</p>
+                     <p className="font-product-sans-m text-[11px]">2006 - 2018</p>
+                  </div>
+               </div>
+               
+            </div>
+            <div class="absolute hidden md:block left-1/2 -ml-0.5 w-0.5 h-[400px] mt-[50px] bg-gray-600"></div>
+            <div className="flex flex-col md:items-start md:ml-[5rem] xs:items-center xs:space-y-8 md:space-y-0">
+               <p className="font-product-sans text-custom-red transition-colors duration-300 hover:text-white xs:text-3xl xs:mt-[50px] md:self-start md:ml-[50px] md:mt-[50px] md:text-3xl">Work Experience</p>
+
+               <div className="flex flex-row md:items-center xs:space-x-5 xs:mr-[70px]">
+               <img src="/images/Tata.jpg" className="md:h-[50px] md:mt-[40px] md:ml-[40px] rounded-full md:p-[6px] xs:h-[40px]" />
+                 <div className="flex flex-col md:ml-[20px] md:mt-[40px]">
+                     <p className="font-product-sans-m ">Tata Elxsi</p>
+                     <p className="font-product-sans-m text-[11px]">2023 - Present</p>
+                  </div>
+               </div>
+
+               <div className="flex flex-row items-center xs:space-x-5 xs:ml-[5px]">
+               <img src="/images/infox.png" className="md:h-[50px] md:mt-[40px] md:ml-[40px]  rounded-full md:p-[6px] xs:h-[40px]" />
+                 <div className="flex flex-col md:ml-[20px] md:mt-[40px]">
+                     <p className="font-product-sans-m ">Infox Technologies</p>
+                     <p className="font-product-sans-m text-[11px]">2021 - 2021</p>
+                  </div>
+               </div>
+            </div>  
+         </div>
+
+         <p className="font-product-sans text-custom-red transition-colors duration-300 hover:text-white xs:self-center xs:text-4xl xs:mt-[50px] md:self-start md:ml-[15rem] md:mt-[150px] md:text-5xl ">Projects</p>
+
+         <div className="flex items-center md:justify-start flex-col md:flex-row md:ml-[30rem] w-full md:space-x-[50px] pb-[10rem]">
+            <div className="flip">
+               <div className="front">
+                  <div className="flex border-2 p-5 rounded-lg mt-[50px] justify-center space-y-5 w-[300px] bg-[#222323]">
+                     <img src="/images/kshree.svg" className="w-[80px] h-[360px]"/>
+                  </div>
+               </div>
+               <div className="back">
+                  <div className="flex flex-col border-2 p-5 rounded-lg mt-[50px] space-y-5 w-[300px] bg-custom-black">
+                     <img src="/images/project-19.png" className="flex w-[300px] h-[150px]"/>
+                     <p className="font-product-sans text-white text-3xl mt-[20px]">Kshree</p>
+                     <p className="font-product-sans-m text-white text-[10px]">User Roles and Access Levels: The app offers distinct logins for Admins and Members, ensuring controlled access to different functionalities and data.</p>
+                     <p className="font-product-sans-m text-white text-[10px]">Attendance Management: Admins can efficiently schedule and mark attendance for Kudumbashree members, ensuring accurate tracking.</p>
+                  </div>
+               </div>
+            </div>
+
+            <div className="flip">
+               <div className="front">
+                  <div className="flex border-2 p-5 rounded-lg mt-[50px] space-y-5 w-[300px] bg-[#222323]">
+                     <img src="/images/rocket.svg" className="w-[300px] h-[360px]"/>
+                  </div>
+               </div>
+               <div className="back">
+                  <div className="flex flex-col border-2 p-5 rounded-lg mt-[50px] space-y-5 w-[300px] pb-[50px] bg-custom-black">
+                     <img src="/images/Astrofilo.png" className="flex w-[250px] h-[200px]"/>
+                     <p className="font-product-sans text-white text-3xl mt-[20px]">Astrofilo</p>
+                     <p className="font-product-sans-m text-white text-[10px]">A React Native App developed and designed to Track Space Launches. It uses the spacedevs API and SpaceX API to get data from servers to display the data</p>
+                  </div>
+               </div>
+            </div>
+
+            <div className="flip">
+               <div className="front">
+                  <div className="flex border-2 p-5 rounded-lg mt-[50px] space-y-5 w-[300px] bg-[#222323] justify-center">
+                     <img src="/images/calcu.svg" className="w-[100px] h-[360px]"/>
+                  </div>
+               </div>
+               <div className="back">
+                  <div className="flex flex-col border-2 p-5 rounded-lg mt-[50px] space-y-5 w-[300px] ">
+                     <img src="/images/cal.png" className="flex w-[250px] h-[250px]"/>
+                     <p className="font-product-sans text-white text-3xl mt-[20px]">MyCalc</p>
+                     <p className="font-product-sans-m text-white text-[10px]">A Native Android App developed and designed to Calculate Numbers, obviously</p>
+                  </div>
+               </div>
+            </div>            
+         </div>
+
+      </div>
+   )
+}
--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,7 @@
 "use client";
 import Image from "next/image"
 import { useState, useEffect } from 'react';
-
+import { useRouter } from "next/navigation";
 
 export default function Home() {
 
@@ -14,18 +14,15 @@ export default function Home() {
 
   }, []);
 
+  const router  = useRouter();
+
   return (
     
     <div className="flex-1 bg-custom-black w-screen h-screen">
     <div className="flex justify-between px-4 xs:ml-[30px]">
       <h4 className="md:pt-16 xs:pt-10 font-product-sans text-custom-red tracking-wide text-3xl xs:text-xl md:text-4xl  xs:justify-start">M3BIONIX</h4>
       <div className="flex pt-16 text-right">
-        <div className="group transition-all duration-300 ease-in-out px-5 md:flex hidden">
-          <button className="bg-left-bottom font-product-sans-m py-3 bg-gradient-to-r from-custom-red to-custom-red bg-[length:0%_2px] bg-no-repeat group-hover:bg-[length:100%_2px] transition-all duration-500 ease-out">
-            About Me
-          </button>
-        </div>
-
+        
         <div className="group transition-all duration-300 ease-in-out px-5 md:flex hidden">
           <button className="bg-left-bottom font-product-sans-m py-3 bg-gradient-to-r from-custom-red to-custom-red bg-[length:0%_2px] bg-no-repeat group-hover:bg-[length:100%_2px] transition-all duration-500 ease-out">
             Contact
@@ -50,7 +47,7 @@ export default function Home() {
             <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center backdrop-blur z-10" onClick={() => setIsOpen(false)}>
                 <div className="transparent rounded-md shadow-lg p-6" onClick={(e) => e.stopPropagation()}>
                     <div className="group transition-all duration-300 ease-in-out px-5 flex">
-                      <button  onClick={() => setIsOpen(false)} className="bg-left-bottom font-product-sans-m py-3 bg-gradient-to-r from-custom-red to-custom-red bg-[length:0%_2px] bg-no-repeat group-hover:bg-[length:100%_2px] transition-all duration-500 ease-out">
+                      <button  onClick={() => {router.push('/about')}} className="bg-left-bottom font-product-sans-m py-3 bg-gradient-to-r from-custom-red to-custom-red bg-[length:0%_2px] bg-no-repeat group-hover:bg-[length:100%_2px] transition-all duration-500 ease-out">
                       About Me
                       </button>
                     </div>
@@ -72,61 +69,49 @@ export default function Home() {
       </div>
 
     </div>
-    <div className="flex flex-col justify-center items-center mt-20 xs:mt-12 lg:mt-32">
-    <div className="md:text-center xs:right-10 flex xs:items-center justify-center md:h-auto ">
-      <p className="font-product-sans text-custom-red xs:text-5xl md:text-7xl xs:mt-[20px] md:mt-10 transition-colors duration-300 hover:text-white">Hey, there</p>        
-    </div>
-      <div className="w-full mt-8 hidden md:flex mx-auto sm:w-1/3 md:w-1/2 md:mr-[180px]">
+    <div className="flex flex-col justify-center items-center mt-20 xs:mt-12 md:mt-20">
+        <div className="md:text-center xs:right-10 flex xs:items-center justify-center md:h-auto ">
+          <p className="font-product-sans text-custom-red xs:text-5xl md:text-7xl xs:mt-[20px] md:mt-10 transition-colors duration-300 hover:text-white">Hey, there</p>        
+        </div>
+        <div className="w-full mt-8 hidden md:flex mx-auto sm:w-1/3 md:w-1/2 md:mr-[180px]">
           <p className="font-product-sans-m md:leading-8 text-left xs:text-sm xs:text-justify md:text-base ">
           I'm Sanjay, fluent in the languages of movies, anime, and questionable code.
           I dabble in philosophy, mostly to fuel my existential dread during ramen nights.
           Think Neo, but with less fighting robots and more existential dread-induced pizza orders. 
           Hire me before I code my own AI girlfriend (no judgment on my waifu preferences).
           </p>
-      </div>
+        </div>
       <div className="md:flex md:justify-between">
-  <div className="w-full mt-8 md:hidden px-4">
-    <p className="font-product-sans-m xs:text-sm xs:text-center xs:justify-around xs:w-auto md:text-base ">
-      {joke}
-    </p>
-  </div>
-  <button type="button" className="hire flex items-center xs:w-auto xs:px-4 xs:py-2 md:py-4 md:px-5 xs:mx-auto xs:text-center md:mr-[15rem]" >
-  <span className="xs:text-sm sm:text-base">Hire Me</span>
-  <svg className="rtl:rotate-180 xs:w-2.5 sm:w-3.5 xs:h-2.5 sm:h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
-    <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
-  </svg>
-</button>
-</div>
-      
-      <div className="gooey absolute md:left-10 md:bottom-10 md:w-[350px] md:h-[400px] xs:flex xs:justify-center xs:left-[7rem] xs:bottom-0 xs:w-[150px] xs:h-[200px]" />
-      <div className="absolute md:left-0 md:bottom-0 md:ml-10 z-2 filter grayscale hover:grayscale-0 transition-all duration-500 xs:flex xs:justify-center xs:left-0 xs:bottom-0 ">
-          <Image src="/images/profile.png" width={350} height={350} className="w-full xs:w-1/2 xs: md:w-auto" alt="Hey, there was supposed to be a image here"/>
+        <div className="w-full mt-8 md:hidden px-4">
+          <p className="font-product-sans-m xs:text-sm xs:text-center xs:justify-around xs:w-auto md:text-base ">
+            {joke}
+          </p>
+        </div>
+        <button type="button" className="hire flex items-center xs:w-auto xs:px-4 xs:py-2 md:py-4 md:px-5 xs:mx-auto xs:text-center md:mr-[10rem]" onClick={()=>{router.push('/about')}}>
+          <span className="xs:text-sm sm:text-base">More About Me</span>
+            <svg className="rtl:rotate-180 xs:w-2.5 sm:w-3.5 xs:h-2.5 sm:h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+              <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
+            </svg>
+        </button>
       </div>
-      <div className="flex justify-center space-x-5 md:self-end md:px-[5rem] md:pt-[4rem] xs:mt-[15px]">
-  <button 
-    type="button" 
-    className="btn flex items-center space-x-3 text-white hover:text-custom-red" 
-    onClick={() => window.open('https://github.com/M3BIONIX', '_blank')}
-  >
-    <Image src="/images/github.png" width={30} height={30} alt="Hey, there was supposed to be a image here"/>
-  </button>
-  <button 
-    type="button" 
-    className="btn flex items-center space-x-3 text-white hover:text-custom-red" 
-    onClick={() => window.open('www.linkedin.com/in/sanjay-mathew34', '_blank')}
-  >
-    <Image src="/images/linkedin.png" width={40} height={40} alt="Hey, there was supposed to be a image here"/>
-  </button>
-  <button 
-    type="button" 
-    className="btn flex items-center space-x-3 text-white hover:text-custom-red" 
-    onClick={() => window.open('https://open.spotify.com/user/cmlmip62j0ll3blad4ub0g937?si=0ad35db74f024930', '_blank')}
-  >
-    <Image src="/images/spotify.png" width={30} height={30} alt="Hey, there was supposed to be a image here"/>
-  </button>
-</div>
+    </div>
+      <div className="gooey absolute md:bottom-[40px] md:left-10 md:w-[350px] md:h-[350px] xs:flex xs:justify-center xs:left-[7rem] xs:bottom-0 xs:w-[150px] xs:h-[200px]" />
+      <div className="absolute md:left-0 md:ml-10 z-2 filter grayscale hover:grayscale-0 transition-all duration-500 xs:flex xs:justify-center xs:left-0 xs:bottom-0 ">
+          <Image src="/images/profile.png" width={280} height={280} className=" md:mt-[50px] xs:w-1/2 md:w-auto" alt="Hey, there was supposed to be a image here"/>
+      </div>
+        <div className="flex flex-row space-x-5 md:justify-end md:mr-[5rem] md:mt-[8rem] xs:justify-center xs:mt-5">
+        <button  type="button"  className="btn flex items-center space-x-3 text-white hover:text-custom-red"   onClick={() => window.open('https://github.com/M3BIONIX', '_blank')}>
+                 <Image src="/images/github.png" width={30} height={30} alt="Hey, there was supposed to be a image here"/>
+        </button>
+        <button type="button"  className="btn flex items-center space-x-3 text-white hover:text-custom-red" onClick={() => window.open('www.linkedin.com/in/sanjay-mathew34', '_blank')}>
+                <Image src="/images/linkedin.png" width={40} height={40} alt="Hey, there was supposed to be a image here"/>
+        </button>
+        <button type="button" className="btn flex items-center space-x-3 text-white hover:text-custom-red" onClick={() => window.open('https://open.spotify.com/user/cmlmip62j0ll3blad4ub0g937?si=0ad35db74f024930', '_blank')}>
+                <Image src="/images/spotify.png" width={30} height={30} alt="Hey, there was supposed to be a image here"/>
+        </button>
+        </div>
 
-  </div> 
+
 </div>
 )
 }
